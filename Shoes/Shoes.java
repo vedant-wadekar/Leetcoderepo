4206fd3fcebb4031c2b48f3fde82024ddcacbc66
vedant-wadekar <vedantwadekar2004@gmail.com>
2026-07-23 17:04:48 +0530

    new add

--- Shoes/Shoes.java
+++ Shoes/Shoes.java
@@ -1,12 +1,15 @@
 public class Shoes{
 public static void main(String[] args){
 String[] str = {"7L","7R","7L","8R","6R","7R","8R","6R"};
+
+for(int i=0;i<str.length;i++){
 int start = 0;
 int end = arr.length-1;
-if(str[start].contains("L") && str[end].contains("R") && str[start].charAt(0).equals(str[end].charAt(0)){
-
-
+while(start<end){
+if(str[start].contains("L") && str[end].contains("R") && (str[start].charAt(0)).equals(str[end].charAt(0)){
 } 
+}
 
+}
 }
 }
